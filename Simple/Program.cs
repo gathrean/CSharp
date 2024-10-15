77f18676cf583ba581dad200e1544b1dfb71540e
Gathrean Dela Cruz <gathrean@icloud.com>
2024-10-14 19:12:59 -0700

    Created a simple program, following a tutorial

---
+++ Simple/Program.cs
@@ -0,0 +1,14 @@
+﻿// Following tutorial from https://www.tutorialsteacher.com/csharp/first-csharp-program
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
